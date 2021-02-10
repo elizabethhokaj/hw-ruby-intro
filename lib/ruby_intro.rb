685abba283965419dda30c54d75804dbec754fc2
elizabethhokaj <78813068+elizabethhokaj@users.noreply.github.com>
2021-02-09 20:39:18 -0600

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,67 @@
-# When done, submit this entire file to the autograder.
+## When done, submit this entire file to the autograder.
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+ return arr.reduce(0, :+)
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(arr)
+  arr = arr.max(2)
+  return arr.inject(0){|sum,x| sum + x}
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(arr, n)
+  return false if arr.empty? && n.zero?
+  if arr.combination(2).any? {|a, b| a + b == n}
+    return true
+  else
+    return false
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+  if /^[^aeiou\W]/i.match(s) == nil
+    return false
+  else
+    return true
+  end
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4?(s)
+  check = s.to_i(2)
+  if s=='' or s=~/[^01]/
+    return false
+  end
+  if check%4 == 0
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+
+  attr_accessor :isbn
+  attr_accessor :price
+  
+  def initialize isbn, price
+    raise ArgumentError.new("ISBN Vlue is Empty") if isbn.empty?()
+      @isbn = isbn
+    raise ArgumentError.new("Price must be greater than 0") if price <= 0
+      @price = price
+  end
+  
+  def price_as_string
+    return "$" + ("%.2f" % price).to_s
+  end
+        
 end
